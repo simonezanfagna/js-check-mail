bd50f8e88cd7335a57c2628917a53cf77f15fbcd
simonezanfagna <zanfagnasimone@live.it>
2019-11-18 10:07:40 +0100

    modifica

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 var mail = prompt('Inserisci la tua mail');
 var lista_mail = ['mario@gmail.com', 'maria@live.it', 'michele@gmail.com', 'asia@gmail.com', 'marco@gmail.com'];
 var testo = "";
+var s = false;
 
 for ( var i = 0; i < lista_mail.length; i++ ) {
   testo = lista_mail[i];
   if (testo == mail) {
     console.log('benvenuto');
     i = lista_mail.length;
-    var s = true;
+    s = true;
     document.getElementById('verifica-mail').innerHTML = 'Benvenuto';
   }
 }
 
-if (!s) {
+if (s == false) {
   console.log('non sei in lista');
   document.getElementById('verifica-mail').innerHTML = 'Non sei in lista';
 }
